6be312a4df18a413740c6b131828651186ca6929
Mahir Tajwar Rahman <121459809+mahirTaj@users.noreply.github.com>
2025-04-19 00:45:26 +0600

    implemented role based redirecting after login, showing error and success message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,11 +16,12 @@ import ShoppingCheckout from "./pages/shopping-view/checkout";
 import ShoppingAccount from "./pages/shopping-view/account";
 import CheckAuth from "./components/common/check-auth";
 import UnauthPage from "./pages/unauth-page";
+import { useSelector } from "react-redux";
 
 function App() {
 
-  const isAuthenticated = false; // Replace with actual authentication logic
-  const user = null
+
+  const{user, isAuthenticated}= useSelector(state=> state.auth )
 
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
